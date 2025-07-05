b82086ecd342c9dee3d1e4d296485f76971af1a8
acejolouise <167699265+acejolouise@users.noreply.github.com>
2025-07-05 15:58:11 +0800

    routing level 4-20

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ import Level2Page from './pages/Level2Page';
 import Level3Page from './pages/Level3Page';
 import Level4Page from './pages/Level4Page';
 import Level5Page from './pages/Level5Page';
+import Level6Page from './pages/Level6Page';
+import Level7Page from './pages/Level7Page';
+import Level8Page from './pages/Level8Page';
+import Level9Page from './pages/Level9Page';
+import Level10Page from './pages/Level10Page';
+import Level11Page from './pages/Level11Page';
+import Level12Page from './pages/Level12Page';
+import Level13Page from './pages/Level13Page';
+import Level14Page from './pages/Level14Page';
+import Level15Page from './pages/Level15Page';
+import Level16Page from './pages/Level16Page';
+import Level17Page from './pages/Level17Page';
+import Level18Page from './pages/Level18Page';
+import Level19Page from './pages/Level19Page';
+import Level20Page from './pages/Level20Page';
 
 
 /* Core CSS required for Ionic components to work properly */
@@ -59,9 +74,52 @@ const App: React.FC = () => (
         <Route exact path="/category/:categoryId/level/5">
           <Level5Page />
         </Route>
-       
-        
- 
+         <Route exact path="/category/:categoryId/level/6">
+          <Level6Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/7">
+          <Level7Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/8">
+          <Level8Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/9">
+          <Level9Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/10">
+          <Level10Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/11">
+          <Level11Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/12">
+          <Level12Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/13">
+          <Level13Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/14">
+          <Level14Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/15">
+          <Level15Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/16">
+          <Level16Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/17">
+          <Level17Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/18">
+          <Level18Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/19">
+          <Level19Page />
+        </Route>
+        <Route exact path="/category/:categoryId/level/20">
+          <Level20Page />
+        </Route>
+
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
---
+++ src/pages/Level19Page.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonButtons,
+  IonBackButton
+} from '@ionic/react';
+import './Level6Page.css';
+
+const Level19Page: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonHeader className="ion-no-border">
+        <IonToolbar className="transparent-toolbar">
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/levels" />
+          </IonButtons>
+          <IonTitle className="ion-text-center">Level 5</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding">      
+        <div className="level1-container">
+          <h2>FSL Numbers</h2>
+          <div className="alphabet-list">
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 11 clicked')}
+            >
+              11
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 12 clicked')}
+            >
+              12
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 13 clicked')}
+            >
+              13
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 14 clicked')}
+            >
+              14
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 15 clicked')}
+            >
+              15
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 16 clicked')}
+            >
+              16
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 17 clicked')}
+            >
+              17
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 18 clicked')}
+            >
+              18
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 19 clicked')}
+            >
+              19
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Number 20 clicked')}
+            >
+              20
+            </button>
+            <button 
+              className="letter-button"
+              onClick={() => console.log('Quiz button clicked')}
+            >
+              Let's see what you remember!
+            </button>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Level19Page;
